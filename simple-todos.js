a3e12ee3f351e5def2a85cefbca71c03ebc02b84
Scully87 <scully87.ts@googlemail.com>
2014-11-29 19:32:34 +0000

    hide completed tasks

--- simple-todos.js
+++ simple-todos.js
@@ -1,19 +1,28 @@
-// tells mongo db to store "tasks"
 Tasks = new Mongo.Collection("tasks");
 
 if (Meteor.isClient) {
-   // This code only runs on the client
+  // This code only runs on the client
   Template.body.helpers({
     tasks: function () {
-      // Show newst tasks first
-      return Tasks.find({}, {sort: {createdAt: -1}});
+      if (Session.get("hideCompleted")) {
+        // If hide completed is checked, filter tasks
+        return Tasks.find({checked: {$ne: true}}, {sort: {createdAt: -1}});
+      } else {
+        // Otherwise, return all of the tasks
+        return Tasks.find({}, {sort: {createdAt: -1}});
+      }
+    },
+    hideCompleted: function () {
+      return Session.get("hideCompleted");
+    },
+    incompleteCount: function () {
+      return Tasks.find({checked: {$ne: true}}).count();
     }
   });
 
   Template.body.events({
     "submit .new-task": function (event) {
       // This function is called when the new task form is submitted
-
       var text = event.target.text.value;
 
       Tasks.insert({
@@ -26,6 +35,9 @@ if (Meteor.isClient) {
 
       // Prevent default form submit
       return false;
+    },
+    "change .hide-completed input": function (event) {
+      Session.set("hideCompleted", event.target.checked);
     }
   });
 
@@ -38,5 +50,4 @@ if (Meteor.isClient) {
       Tasks.remove(this._id);
     }
   });
-
 }
